b770220e459948f145c3c5bad73bc21b60af1b87
Siddharth Patel <sid.auburn@gmail.com>
2026-02-23 21:10:57 -0500

    Refactor AWS resources and configurations for improved load balancing and security group settings

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ data "aws_vpc" "default" {
 data "aws_subnets" "default" {
   filter {
     name = "vpc-id"
-    values = [ data.aws_vpc.default ]
+    values = [ data.aws_vpc.default.id ]
   }
 }
 
@@ -24,7 +24,7 @@ variable "server_port" {
 resource "aws_launch_configuration" "my-sample-instance" {
   image_id = "ami-0b6c6ebed2801a5cb"
   instance_type = "t2.micro"
-  security_groups = [ aws_security_group.instance-security-group ]
+  security_groups = [ aws_security_group.instance-security-group.id ]
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World!" > index.html
@@ -39,6 +39,8 @@ resource "aws_autoscaling_group" "my-sample-instance-asg" {
   vpc_zone_identifier = data.aws_subnets.default.ids
   min_size = 2
   max_size = 3
+  target_group_arns = [aws_lb_target_group.asg-target-group.arn]
+  health_check_type = "ELB"
   tag {
     key = "Name"
     value = "terraform-test"
@@ -61,34 +63,78 @@ resource "aws_security_group" "instance-security-group" {
   }
 }
 
+# Load balancer security group to allow incoming traffic on port 80 and allow outgoing traffic to the EC2 instances in the auto scaling group
+resource "aws_security_group" "alb-security-group" {
+  name = "terraform-alb-security-group"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+}
+
 # load balance to distribute traffic to the EC2 instances in the auto scaling group
 resource "aws_lb" "my-sample-lb" {
   name               = "my-sample-lb"
   internal           = false
   load_balancer_type = "application"
   subnets            = data.aws_subnets.default.ids
+  security_groups    = [ aws_security_group.alb-security-group.id ]
 }
 
 resource "aws_lb_listener" "my-sample-listener" {
   load_balancer_arn = aws_lb.my-sample-lb.arn
-  port              = var.server_port
+  port              = 80
   protocol          = "HTTP"
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.my-sample-tg.arn
+    type             = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      status_code = 404
+      message_body = "404: Page Not Found"
+    }
   }
 }
 
-resource "aws_lb_target_group" "my-sample-tg" {
+resource "aws_lb_target_group" "asg-target-group" {
   name     = "my-sample-tg"
   port     = var.server_port
   protocol = "HTTP"
   vpc_id   = data.aws_vpc.default.id
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
 }
 
+resource "aws_lb_listener_rule" "my-sample-listener-rule" {
+  listener_arn = aws_lb_listener.my-sample-listener.arn
+  priority     = 100
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg-target-group.arn
+  }
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+  
+}
 
-
-# output "instance_public_ip" {
-#   description = "output details of the sample ec2 instance"
-#   value = aws_instance.my-sample-instance.public_ip
-# }
+output "alb_dns_name" {
+  description = "DNS name of the application load balancer"
+  value = aws_lb.my-sample-lb.dns_name
+}
